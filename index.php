094baf175b821974ac1fd98593fa1022185549f6
amanda.fager@outlook.com <amanda.fager@outlook.com>
2020-10-19 17:07:09 +0200

    Created files

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+// This is the file where you can keep your HTML markup. We should always try to
+// keep us much logic out of the HTML as possible. Put the PHP logic in the top
+// of the files containing HTML or even better; in another PHP file altogether.
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
