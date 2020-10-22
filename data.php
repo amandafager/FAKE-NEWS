83bc7fe22705f8cf20489a2ded85d7333e0e031b
amanda.fager@outlook.com <amanda.fager@outlook.com>
2020-10-22 23:40:17 +0200

    addad a foreach that loops all posts with value 'culture'

--- data.php
+++ data.php
@@ -124,7 +124,6 @@ $posts =
         [
             'id' => 5,
             'title' => 'Standing Up in the Milky Way',
-
             'published_date' => '2020-10-02 11:20',
             'likes' => 20,
             'content' => "Hey George, heard you laid out Biff, nice going. Hey Marty, I'm not your answering service, but you're outside pouting about the car, Jennifer Parker called you twice. You cost three-hundred buck damage to my car, you son-of-a-bitch. And I'm gonna take it out of your ass. Hold him. Good, I'll see you tonight. Don't forget, now, 1:15 a.m., Twin Pines Mall. Because, you might regret it later in life. Biff, stop it. Biff, you're breaking his arm. Biff, stop. What, right here right now in the cafeteria? What if she said no? I don't know if I could take that kind of rejection. Besides, I think she'd rather go with somebody else. Nothing's coming to my mind. Good, I'll see you tonight. Don't forget, now, 1:15 a.m., Twin Pines Mall. Yeah well, I saw it on a rerun. I guess you guys aren't ready for that yet. But your kids are gonna love it. Oh, oh a rematch, why, were you cheating? Yeah. You too. Uh, look me up when you get there.",
